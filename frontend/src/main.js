808d8d8f218c27260046af42cb6b78a3be0b7be5
wenhao801 <wenhao801@126.com>
2025-11-04 20:19:36 +0800

    Feat(frontend):添加登录，美化了navbar

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./style.css";
 import router from "./router"; 
+import { initAuth } from "./auth";
+
+// 初始化本地存储中的认证状态
+initAuth();
 
 const app = createApp(App);
 app.use(router); // ✅ 启用路由
